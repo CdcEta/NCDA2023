90e6ce745056506b56a6f46b3e020ffcc421c4cc
JcMarical <1109361945@qq.com>
2023-06-17 23:40:17 +0800

    乌鸦

--- Assets/3.Script/Background/Bird.cs
+++ Assets/3.Script/Background/Bird.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     private Animator anim;
+    public bool stopMove;   
     private bool isFly=false;
     public float horizontalSpeed;
     public float verticalSpeed;
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+        if (stopMove)
+        {
+            anim.speed = 0;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        anim.speed = 1;
         anim.SetTrigger("Flying");
             isFly = true;
         Destroy(gameObject, 5f);
     }
 
-    private void Start()
-    {
-        anim= GetComponent<Animator>();
-    }
+
+ 
     private void Update()
     {
         if(isFly)
