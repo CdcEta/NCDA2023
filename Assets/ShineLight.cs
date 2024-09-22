ef49dbcf7ea4d08ef639b2007ef6ccb612267763
Maric <1109361945@qq.com>
2024-09-22 17:27:59 +0800

    项目最终

--- Assets/ShineLight.cs
+++ Assets/ShineLight.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.Universal;
+
 
 public class ShineLight : MonoBehaviour
 {
@@ -10,11 +10,11 @@ public class ShineLight : MonoBehaviour
     public float outSpeed = 1;
     private void Start()
     {
-        StartCoroutine(TextAppear(transform.GetComponent<Light2D>()));
+        StartCoroutine(TextAppear(transform.GetComponent<UnityEngine.Rendering.Universal.Light2D>()));
         // Destroy(GameObject.Find("menu"));
     }
     // Start is called before the first frame update
-    private IEnumerator TextAppear(Light2D t)
+    private IEnumerator TextAppear(UnityEngine.Rendering.Universal.Light2D t)
     {
         while (t.intensity<maxintensity)
         {
